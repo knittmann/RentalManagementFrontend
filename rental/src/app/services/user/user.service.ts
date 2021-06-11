12235b84475f4594db254c36d190816db2f43a0b
Krista Nittmann <cieloangel@cox.net>
2021-06-11 15:22:24 -0500

    Removed upper level Node js node_modules folder

--- rental/src/app/services/user/user.service.ts
+++ rental/src/app/services/user/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 
 import { User } from '../../models/User';
 
